ce67946a616e7548abb85351f148aee9fe62167e
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2021-11-11 12:38:26 +0100

    aktualizacja moduł_07

--- poziom_podstawowy/Modul_07/nwd_01.py
+++ poziom_podstawowy/Modul_07/nwd_01.py
@@ -1,11 +1,11 @@
 def nwd_odejmowanie(x, y):
-	while x != y:
-		if x > y:
-			x -= y
-		else:
-			y -= x
+    while x != y:
+        if x > y:
+            x -= y
+        else:
+            y -= x
 
-	return x
+    return x
 
 # przykład wywołania:
 # nwd_odejmowanie(23849876, 34)
--- poziom_podstawowy/Modul_07/nwd_02_test.py
+++ poziom_podstawowy/Modul_07/nwd_02_test.py
@@ -1,10 +1,10 @@
 def nwd_modulo(x, y):
-	while y > 0:
-		modulo = x % y
-		x = y
-		y = modulo
+    while y > 0:
+        modulo = x % y
+        x = y
+        y = modulo
 
-	return x
+    return x
 
 # przykład wywołania:
 nwd_modulo(23849876, 34)
